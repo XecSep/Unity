d1d0ab9e2f8a2f68c79a496544481af9f5d40f53
KHS <kij4300@naver.com>
2018-12-17 18:52:44 +0900

    배경 움직이기, 충돌처리
    배경을 움직이게 했다.
    총알과 적이 충돌하면 적이 파괴되게 했다.

---
+++ 2D Shooting/Assets/Script/BackGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public float scrollSpeed = 1.5f;
+
+    Material mt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mt = GetComponent<Renderer>().material;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float newOffsetY = mt.mainTextureOffset.y + scrollSpeed * Time.deltaTime;
+
+        Vector2 newOffset = new Vector2(0, newOffsetY);
+
+        mt.mainTextureOffset = newOffset;
+    }
+}
