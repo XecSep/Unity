cb774968f7d0d74a98d6242bee670e2ea8b638b9
KHS <kij4300@naver.com>
2018-12-28 22:02:28 +0900

    FirePos 만듬. Bullet을 스프라이트 처리함.
    FirePos를 만들었다.
    Bullet을 Sprite로 만들었다.

---
+++ 2D Shooting/Assets/Script/Bullet/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform tr;
+
+    public float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        tr.Translate(Vector2.up * moveSpeed * Time.deltaTime, Space.Self);
+    }
+}
